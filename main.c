6c3daebb322b09e898b12d8904b2c997f8792ac8
waterlens <waterlens@waterlens.moe>
2025-01-29 12:20:35 +0800

    modify

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 
 #include "hashtbl.h"
 
-#define INT63_EQ(a, b) (*(const int64_t *)(a) == (b)->key)
-#define INT63_HASH(x)                                                          \
+#define INT64_EQ(a, b) (*(const int64_t *)(a) == (b)->key)
+#define INT64_HASH(x)                                                          \
   (hashtbl_default_hash((const int64_t *)(x), sizeof(int64_t)))
-HASHMAP_NEW_KIND(i63map, int64_t, int64_t, 8, DEFAULT_ALLOC, DEFAULT_COPY,
-                 DEFAULT_DEL, INT63_EQ, DEFAULT_FREE, DEFAULT_GET, INT63_HASH,
+HASHMAP_NEW_KIND(i64map, int64_t, int64_t, 8, DEFAULT_ALLOC, DEFAULT_COPY,
+                 DEFAULT_DEL, INT64_EQ, DEFAULT_FREE, DEFAULT_GET, INT64_HASH,
                  DEFAULT_INIT, DEFAULT_MOVE)
 
 int main() {
-  i63map_t *map = i63map_new(10);
+  i64map_t *map = i64map_new(10);
 
-  size_t s = i63map_size(map);
+  size_t s = i64map_size(map);
   assert(s == 0);
 
   int64_t keys[][2] = {
@@ -22,22 +22,22 @@ int main() {
   };
 
   for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); i++) {
-    i63map_insert_t ins = i63map_insert(&map, &keys[i][0]);
-    i63map_entry_t *entry = i63map_iter_get(&ins.iter);
+    i64map_insert_t ins = i64map_insert(&map, &keys[i][0]);
+    i64map_entry_t *entry = i64map_iter_get(&ins.iter);
     entry->key = keys[i][0];
     entry->val = keys[i][1];
   }
 
-  s = i63map_size(map);
+  s = i64map_size(map);
   assert(s == 10);
 
   for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); i++) {
-    i63map_iter_t iter = i63map_find(map, &keys[i][0]);
-    i63map_entry_t *entry = i63map_iter_get(&iter);
+    i64map_iter_t iter = i64map_find(map, &keys[i][0]);
+    i64map_entry_t *entry = i64map_iter_get(&iter);
     assert(entry->key == keys[i][0]);
     assert(entry->val == keys[i][1]);
   }
 
-  i63map_destroy(map);
+  i64map_destroy(map);
   return 0;
 }
